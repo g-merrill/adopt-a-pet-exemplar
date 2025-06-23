305e5643023f4512c2daea7c1d87df27696d265b
Greg Merrill <gmerrill03@gmail.com>
2025-06-23 15:49:18 -0700

    figure out ejs/common import/require nonsense

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,32 +1,57 @@
-const { PrismaClient } = require('@prisma/client')
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+
+const { PrismaClient } = require("./generated/prisma-client/index")
+const bcrypt = require("bcrypt")
+
 const prisma = new PrismaClient()
 
+const hashedPassword = await bcrypt.hash("password1234", 10)
+
+const users = [
+	{
+		username: "gmerrill",
+		password: hashedPassword,
+	},
+]
+
 const pets = [
-    { id: 1, name: 'tycho', type: 'dog', age: 2 },
-    { id: 2, name: 'mika', type: 'dog', age: 1 },
-    { id: 3, name: 'stringer', type: 'dog', age: 10 },
-];
+	{ id: 1, name: "tycho", type: "dog", age: 2 },
+	{ id: 2, name: "mika", type: "dog", age: 1 },
+	{ id: 3, name: "stringer", type: "dog", age: 10 },
+]
 
 async function main() {
-    console.log(`Start seeding ...`)
+	console.log(`Start seeding ...`)
 
-    // Clear the table first
-    await prisma.pet.deleteMany()
-    
-    for (const pet of pets) {
-        const petRecord = await prisma.pet.create({
-            data: pet,
-        })
-        console.log(`Created pet with id: ${petRecord.id}`)
-    }
-    console.log(`Seeding finished.`)
+	// Clear the table first
+	await prisma.pet.deleteMany()
+	await prisma.user.deleteMany()
+
+	let userId
+
+	for (const user of users) {
+		const userRecord = await prisma.user.create({
+			data: user,
+		})
+		userId = userRecord.id
+		console.log(`Created user with id: ${userRecord.id}`)
+	}
+
+	for (const pet of pets) {
+		const petRecord = await prisma.pet.create({
+			data: { ...pet, userId },
+		})
+		console.log(`Created pet with id: ${petRecord.id}`)
+	}
+	console.log(`Seeding finished.`)
 }
 
 main()
-    .catch((e) => {
-        console.error(e)
-        process.exit(1)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+	.catch((e) => {
+		console.error(e)
+		process.exit(1)
+	})
+	.finally(async () => {
+		await prisma.$disconnect()
+	})
--- server/routes/starter.js
+++ server/routes/starter.js
@@ -1,20 +1,58 @@
-const express = require('express')
+const express = require("express")
+const bcrypt = require("bcrypt")
+const prisma = require("../prisma/prisma-client.js")
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Welcome to my app!')
+router.get("/", (req, res) => {
+	res.send("Welcome to my app!")
 })
 
-router.get('/sample-path', (req, res) => {
-    res.send('This is a sample response.')
+router.post("/signup", async (req, res) => {
+	const { username, password } = req.body
+	if (!username || !password) {
+		return res
+			.status(400)
+			.json({ error: "Username and password are required." })
+	}
+
+	if (password.length < 8) {
+		return res
+			.status(400)
+			.json({ error: "Password must be at least 8 characters long." })
+	}
+	const existingUser = await prisma.user.findUnique({
+		where: { username },
+	})
+
+	if (existingUser) {
+		return res.status(400).json({ error: "Username already taken." })
+	}
+	const hashedPassword = await bcrypt.hash(password, 10)
+	const newUser = await prisma.user.create({
+		data: { username, password: hashedPassword },
+	})
+	res.status(201).json({ message: `User: ${newUser.username} created successfully!` })
 })
 
-router.get('/hello-world', (req, res) => {
-    res.send('Hello, World!')
+router.post("/login", (req, res) => {
+	res.send("Welcome to my app!")
 })
 
-router.get('/hello-pet', (req, res) => {
-    res.send('Hello, Pet!')
+router.post("/logout", (req, res) => {
+	res.send("Welcome to my app!")
+})
+
+router.get("/sample-path", (req, res) => {
+	res.send("This is a sample response.")
+})
+
+router.get("/hello-world", (req, res) => {
+	res.send("Hello, World!")
+})
+
+router.get("/hello-pet", (req, res) => {
+	res.send("Hello, Pet!")
 })
 
 module.exports = router
